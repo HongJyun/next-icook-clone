3cc2bf333ab163f41bcda69192957ed02076d39b
Frank Lin <hongjyunlin@gmail.com>
2023-12-06 18:19:55 +0800

    fix: fix wrongly import

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { KeywordContent } from "@/app/api/home/route";
+import { KeywordContent } from "@/types";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
