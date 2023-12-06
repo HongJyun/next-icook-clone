1c9173a5732463bc793291f40837494a6937328a
Frank Lin <hongjyunlin@gmail.com>
2023-12-06 20:11:01 +0800

    fix: fix build issue

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,10 @@
-import { HomeData } from "@/app/api/home/route";
+import { GET, HomeData } from "@/app/api/home/route";
 import { SearchResultData } from "@/app/api/search/route";
 import { RecipeDetail } from "@/types";
 
 export const baseUrl = process.env.API_BASE_URL;
 export const getHomeData = async (): Promise<HomeData> => {
-  const res = await fetch(`${baseUrl}/api/home`);
+  const res = GET();
   return res.json();
 };
 
